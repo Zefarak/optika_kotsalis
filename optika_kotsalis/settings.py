4286f2d0c96abd5aaa9d4d9b27e40139cd11708a
chris <lirageika#hotmail.gr>
2019-10-11 20:23:10 +0300

    added https

--- optika_kotsalis/settings.py
+++ optika_kotsalis/settings.py
@@ -22,6 +22,13 @@ else:
     ALLOWED_HOSTS = ['*']
 
 
+if PRODUCTION:
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 # Application definition
 
 INSTALLED_APPS = [
