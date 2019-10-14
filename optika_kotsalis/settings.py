7810590c21ef80f1db041dc18d42bebaace4417b
chris <lirageika#hotmail.gr>
2019-10-14 15:02:05 +0300

    eimai malaks

--- optika_kotsalis/settings.py
+++ optika_kotsalis/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Fa
+DEBUG = False
 PRODUCTION = True
 
 if PRODUCTION:
@@ -27,6 +27,7 @@ if PRODUCTION:
     CSRF_COOKIE_SECURE = True
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
