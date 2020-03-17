3976c0d949a62cf49db37cd79f04ad9aad3b0d30
chris <lirageika#hotmail.gr>
2020-03-17 20:53:17 +0200

    rerf

--- optika_kotsalis/urls.py
+++ optika_kotsalis/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +29,8 @@ urlpatterns = [
 
 
     path('social-auth/', include('social_django.urls', namespace="social")),
+
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), )
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
